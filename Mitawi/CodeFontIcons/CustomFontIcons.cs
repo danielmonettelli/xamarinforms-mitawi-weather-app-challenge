f17d1c2c4a3e733ffb68aab37bada0f98b169321
Daniel Angel Monettelli L <danielmonetelli@hotmail.com>
2021-11-05 18:52:12 -0500

    💄 [Code] Update MonettelliUIKIT c2

--- Mitawi/CodeFontIcons/CustomFontIcons.cs
+++ Mitawi/CodeFontIcons/CustomFontIcons.cs
@@ -6,12 +6,11 @@ namespace Mitawi.CodeFontIcons
 {
     internal static class CustomFontIcons
     {
-        public const string icon_picker_solid = "\ue800";
-        public const string icon_point_solid = "\ue801";
-        public const string icon_wind_outline = "\ue802";
-        public const string icon_arrow_forward_outline = "\ue803";
-        public const string icon_calendar_outline = "\ue804";
-        public const string icon_degree_symbol_outline = "\ue805";
-        public const string icon_location_solid = "\ue806";
+        public const string icon_arrow_forward_outline = "\ue800";
+        public const string icon_calendar_outline = "\ue801";
+        public const string icon_degree_symbol_outline = "\ue802";
+        public const string icon_location_solid = "\ue803";
+        public const string icon_picker_solid = "\ue804";
+        public const string icon_wind_outline = "\ue805";
     }
 }
