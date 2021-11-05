87558ad84033a7bb4c025a69f1945d3d7a2218e9
Daniel Angel Monettelli L <danielmonetelli@hotmail.com>
2021-11-05 11:48:05 -0500

    💄 [Code] Add MonettelliUIKIT c1

--- Mitawi/AssemblyInfo.cs
+++ Mitawi/AssemblyInfo.cs
@@ -1,3 +1,9 @@
+using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 [assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+
+#region FontIcons Embedded
+// CUSTOM FONT ICONS WITH "IcoMoon" and "Fontello"
+[assembly: ExportFont("customfonticons.ttf", Alias = "CustomFontIcons")]
+#endregion
---
+++ Mitawi/CodeFontIcons/CustomFontIcons.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mitawi.CodeFontIcons
+{
+    internal static class CustomFontIcons
+    {
+        public const string icon_picker_solid = "\ue800";
+        public const string icon_point_solid = "\ue801";
+        public const string icon_wind_outline = "\ue802";
+        public const string icon_arrow_forward_outline = "\ue803";
+        public const string icon_calendar_outline = "\ue804";
+        public const string icon_degree_symbol_outline = "\ue805";
+        public const string icon_location_solid = "\ue806";
+    }
+}
