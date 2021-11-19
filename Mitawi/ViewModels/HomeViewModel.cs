414e5be0837892175d29eec94f5e15bc297d927c
Daniel Angel Monettelli L <danielmonetelli@hotmail.com>
2021-11-19 12:08:28 -0500

    🚀 [Code] Add basic pattern MVVM s1

---
+++ Mitawi/ViewModels/HomeViewModel.cs
@@ -0,0 +1,41 @@
+﻿using Mitawi.Models;
+using Mitawi.Services;
+using MvvmHelpers;
+
+namespace Mitawi.ViewModels
+{
+    public class HomeViewModel : BaseViewModel
+    {
+        private readonly RestService _restService;
+
+        private WeatherData _weatherData;
+
+        public WeatherData WeatherData
+        {
+            get => _weatherData;
+            set { _weatherData = value; OnPropertyChanged(); }
+        }
+
+        public HomeViewModel()
+        {
+            _restService = new RestService();
+            WeatherData = new WeatherData();
+            OnGetWeatherData();
+        }
+
+        private async void OnGetWeatherData()
+        {
+            WeatherData = await _restService.GetWeatherData(GenerateRequestUri(Constants.Constants.OpenWeatherMapEndpoint));
+        }
+
+        private string GenerateRequestUri(string endpoint)
+        {
+            string requestUri = endpoint;
+            requestUri += $"?lat=37.5657";
+            requestUri += $"&lon=126.978";
+            requestUri += "&exclude=current,minutely,alerts&units=metric"; // or exclude
+            requestUri += $"&appid={Constants.Constants.OpenWeatherMapAPIKey}";
+            return requestUri;
+        }
+    }
+}
--- Mitawi/Views/HomePage.xaml.cs
+++ Mitawi/Views/HomePage.xaml.cs
@@ -1,6 +1,6 @@
 ﻿using Mitawi.Models;
 using Mitawi.Services;
-
+using Mitawi.ViewModels;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -9,30 +9,11 @@ namespace Mitawi.Views
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class HomePage : ContentPage
     {
-        private readonly RestService _restService;
-
         public HomePage()
         {
             InitializeComponent();
 
-            _restService = new RestService();
-            HeyRest();
-        }
-
-        private async void HeyRest()
-        {
-            WeatherData weatherData = await _restService.GetWeatherData(GenerateRequestUri(Constants.Constants.OpenWeatherMapEndpoint));
-            BindingContext = weatherData;
-        }
-
-        private string GenerateRequestUri(string endpoint)
-        {
-            string requestUri = endpoint;
-            requestUri += $"?lat=38.7267";
-            requestUri += $"&lon=-9.1403";
-            requestUri += "&exclude=current,minutely,alerts&units=metric"; // or exclude
-            requestUri += $"&appid={Constants.Constants.OpenWeatherMapAPIKey}";
-            return requestUri;
+            BindingContext = new HomeViewModel();
         }
     }
 }
