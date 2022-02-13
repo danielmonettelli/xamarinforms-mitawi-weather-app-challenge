2958008952c32aad7dfc927cfec28d5935ea19b5
Daniel Angel Monettelli L <danielmonetelli@hotmail.com>
2022-02-13 14:12:43 -0500

    🚀 [Code] Update HomeDetailPage s7
    ✨ Add 'HomeDetailPageOrientation.xaml' c1.
    🚑️ Critical hotfix on 'Weekly Weather' items display limits for small screens.

--- Mitawi/Views/HomeDetailPage.xaml.cs
+++ Mitawi/Views/HomeDetailPage.xaml.cs
@@ -1,5 +1,4 @@
 ﻿using Mitawi.Utility;
-
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -8,11 +7,26 @@ namespace Mitawi.Views
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class HomeDetailPage : ContentPage
     {
+
         public HomeDetailPage()
         {
             InitializeComponent();
 
+            SizeChanged += HomeDetailPage_Orientation;
+
             BindingContext = ViewModelLocator.HomeDetailViewModel;
         }
+
+        private void HomeDetailPage_Orientation(object sender, System.EventArgs e)
+        {
+            string visualStateOrientation = Height > Width ? "Portrait" : "Landscape";
+
+            VisualStateManager.GoToState(gridParent, visualStateOrientation);
+            VisualStateManager.GoToState(topLayerLower1, visualStateOrientation);
+            VisualStateManager.GoToState(topLayerHigher2, visualStateOrientation);
+            VisualStateManager.GoToState(imageTomorrowWeather, visualStateOrientation);
+            VisualStateManager.GoToState(scrollWeeklyWeather, visualStateOrientation);
+        }
+
     }
 }
---
+++ Mitawi/Views/Orientations/HomeDetailPageOrientation.xaml.cs
@@ -0,0 +1,15 @@
+﻿
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Mitawi.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class HomeDetailPageOrientation : ResourceDictionary
+    {
+        public HomeDetailPageOrientation()
+        {
+            InitializeComponent();
+        }
+    }
+}
